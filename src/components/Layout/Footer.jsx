cfd7457654e93bb7be56960aae50d326f6492027
g4trader <g4trader.news@gmail.com>
2025-08-04 15:46:34 -0300

    Revert "ajuste links redes sociais"
    This reverts commit 44040f06a5e693d645eb34292eb343e123e42ccf.

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -22,13 +22,13 @@ const Footer = () => {
             </p>
 			  
             <div className="flex space-x-4">
-              <a href="https://www.tiktok.com/@plenndigital" className="text-gray-400 hover:text-blue-400 transition-colors">
-                <Tiktok className="h-5 w-5" />
+              <a href="#" className="text-gray-400 hover:text-blue-400 transition-colors">
+                <Linkedin className="h-5 w-5" />
               </a>
-              <a href="https://www.instagram.com/plenndigital" className="text-gray-400 hover:text-pink-400 transition-colors">
+              <a href="#" className="text-gray-400 hover:text-pink-400 transition-colors">
                 <Instagram className="h-5 w-5" />
               </a>
-              <a href="https://www.facebook.com/share/1BE3Wh8VK4/?mibextid=wwXIfr" className="text-gray-400 hover:text-blue-600 transition-colors">
+              <a href="#" className="text-gray-400 hover:text-blue-600 transition-colors">
                 <Facebook className="h-5 w-5" />
               </a>
             </div>
